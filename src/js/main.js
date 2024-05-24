6035556cfad106daa440c0053f880d22820288a5
MaxKrch <144154119+MaxKrch@users.noreply.github.com>
2024-05-25 02:12:05 +0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import myApp from './App.js';
 
-const baseUrl = "my-little-chat.onrender.com:10000";
+const baseUrl = "my-little-chat.onrender.com";
 // const baseUrl = "192.168.1.108:7070";
 
 const app = new myApp('#app', baseUrl); 
